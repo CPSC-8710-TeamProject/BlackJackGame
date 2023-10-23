5d28ed292a52ce2a966293510303edeb656c31eb
Geethakrishna Puligundla <gpuligundla@clemson.edu>
2023-10-23 08:34:32 -0400

    CPSC8710: Added Deal  button

--- script.js
+++ script.js
@@ -85,3 +85,40 @@ function BJstand(){
         }, 800); 
     }
 }
+
+// Deal Button
+document.querySelector('#deal').addEventListener('click', BJdeal);
+
+function BJdeal(){
+
+    if(You['score']=== 0){
+        alert('Please Hit Some Cards First!');
+    }
+    else if(Dealer['score']===0){
+        alert('Please Press Stand Key Before Deal...');
+    }
+    else{
+
+    let yourimg = document.querySelector('#your-box').querySelectorAll('img');
+    let dealerimg = document.querySelector('#dealer-box').querySelectorAll('img');
+    
+    for(let i=0; i<yourimg.length; i++){
+        yourimg[i].remove();
+    }
+    for(let i=0; i<dealerimg.length; i++){
+        dealerimg[i].remove();
+    }
+
+    BJgame['cards'] = ['2C','3C','4C','5C','6C','7C','8C','9C','10C','KC','QC','JC','AC','2D','3D','4D','5D','6D','7D','8D','9D','10D','KD','QD','JD','AD','2H','3H','4H','5H','6H','7H','8H','9H','10H','KH','QH','JH','AH','2S','3S','4S','5S','6S','7S','8S','9S','10S','KS','QS','JS','AS'];
+
+    You['score'] = 0;
+    document.querySelector(You['scoreSpan']).textContent = You['score'];
+    document.querySelector(You['scoreSpan']).style.color = 'whitesmoke';
+    Dealer['score'] = 0;
+    document.querySelector(Dealer['scoreSpan']).textContent = Dealer['score'];
+    document.querySelector(Dealer['scoreSpan']).style.color = 'whitesmoke';
+
+    document.querySelector('#command').textContent = "Let's Play";
+    document.querySelector('#command').style.color = 'black';
+    }
+}
